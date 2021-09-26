d175dfc6522ec3e4dc2a69580b602b171797c8b8
andochiwa <a1066079469@gmail.com>
2021-09-27 04:34:39 +0900

    [feat] global exception handle

---
+++ mall-common/common_utils/src/main/kotlin/com/github/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,21 @@
+package com.github.exception
+
+import com.github.dto.ResultDto
+import com.github.dto.resultError
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+/**
+ * @author Andochiwa
+ * @version 1.0
+ * @since 09-27-04:26
+ */
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception::class)
+    suspend fun globalException(e: Exception): ResultDto {
+        e.printStackTrace()
+        return resultError()
+    }
+}
