edd07a99e2c5354740f342df7e3199feb646d4c1
andochiwa <a1066079469@gmail.com>
2021-10-22 19:23:20 +0900

    [feat] member level validate & crud

--- mall-service/mall-member/src/main/kotlin/com/github/member/entity/MemberLevel.kt
+++ mall-service/mall-member/src/main/kotlin/com/github/member/entity/MemberLevel.kt
@@ -4,13 +4,19 @@ import cn.hutool.core.util.IdUtil
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
+import com.github.vaild.AddGroup
+import com.github.vaild.UpdateGroup
 import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
+import org.hibernate.validator.constraints.Range
 import org.springframework.data.annotation.Id
 import org.springframework.data.domain.Persistable
 import org.springframework.data.relational.core.mapping.Table
 import java.io.Serializable
 import java.math.BigDecimal
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Null
 
 /**
  *
@@ -20,50 +26,63 @@ import java.math.BigDecimal
  */
 @Table("ums_member_level")
 @ApiModel
-data class MemberLevel (
+data class MemberLevel(
 
-	@ApiModelProperty(value = "id")
-	@Id
-	@get:JvmName("deprecate")
-	@JsonSerialize(using = ToStringSerializer::class)
-	var id: Long? = null,
+    @ApiModelProperty(value = "id")
+    @Id
+    @get:JvmName("deprecate")
+    @JsonSerialize(using = ToStringSerializer::class)
+    @field:Null(groups = [AddGroup::class])
+    @field:NotNull(groups = [UpdateGroup::class])
+    var id: Long? = null,
 
-	@ApiModelProperty(value = "等级名称")
-	var name: String? = null,
+    @ApiModelProperty(value = "等级名称")
+    @field:NotBlank(groups = [AddGroup::class, UpdateGroup::class])
+    var name: String? = null,
 
-	@ApiModelProperty(value = "等级需要的成长值")
-	var growthPoint: Int? = null,
+    @ApiModelProperty(value = "等级需要的成长值")
+    @field:NotNull(groups = [AddGroup::class, UpdateGroup::class])
+    var growthPoint: Int? = null,
 
-	@ApiModelProperty(value = "是否为默认等级[0->不是；1->是]")
-	var defaultStatus: Int? = null,
+    @ApiModelProperty(value = "是否为默认等级[0->不是；1->是]")
+    @field:Range(min = 0, max = 1, groups = [AddGroup::class, UpdateGroup::class])
+    @field:NotNull(groups = [AddGroup::class, UpdateGroup::class])
+    var defaultStatus: Int? = null,
 
-	@ApiModelProperty(value = "免运费标准")
-	var freeFreightPoint: BigDecimal? = null,
+    @ApiModelProperty(value = "免运费标准")
+    var freeFreightPoint: BigDecimal? = null,
 
-	@ApiModelProperty(value = "每次评价获取的成长值")
-	var commentGrowthPoint: Int? = null,
+    @ApiModelProperty(value = "每次评价获取的成长值")
+    @field:NotNull(groups = [AddGroup::class, UpdateGroup::class])
+    var commentGrowthPoint: Int? = null,
 
-	@ApiModelProperty(value = "是否有免邮特权")
-	var privilegeFreeFreight: Int? = null,
+    @ApiModelProperty(value = "是否有免邮特权")
+    @field:NotNull(groups = [AddGroup::class, UpdateGroup::class])
+    var privilegeFreeFreight: Int? = null,
 
-	@ApiModelProperty(value = "是否有会员价格特权")
-	var privilegeMemberPrice: Int? = null,
+    @ApiModelProperty(value = "是否有会员价格特权")
+    @field:Range(min = 0, max = 1, groups = [AddGroup::class, UpdateGroup::class])
+    @field:NotNull(groups = [AddGroup::class, UpdateGroup::class])
+    var privilegeMemberPrice: Int? = null,
 
-	@ApiModelProperty(value = "是否有生日特权")
-	var privilegeBirthday: Int? = null,
+    @ApiModelProperty(value = "是否有生日特权")
+    @field:Range(min = 0, max = 1, groups = [AddGroup::class, UpdateGroup::class])
+    @field:NotNull(groups = [AddGroup::class, UpdateGroup::class])
+    var privilegeBirthday: Int? = null,
 
-	@ApiModelProperty(value = "备注")
-	var note: String? = null,
+    @ApiModelProperty(value = "备注")
+    var note: String? = null,
 
 
-) : Serializable, Persistable<Long> {
-	@JsonIgnore
-	override fun isNew(): Boolean {
-		return if (id == null) {
-			id = IdUtil.getSnowflake().nextId()
-			true
-		} else false
-	}
-	@JsonSerialize(using = ToStringSerializer::class)
-	override fun getId(): Long? = id
+    ) : Serializable, Persistable<Long> {
+    @JsonIgnore
+    override fun isNew(): Boolean {
+        return if (id == null) {
+            id = IdUtil.getSnowflake().nextId()
+            true
+        } else false
+    }
+
+    @JsonSerialize(using = ToStringSerializer::class)
+    override fun getId(): Long? = id
 }
--- mall-service/mall-member/src/main/kotlin/com/github/member/service/MemberLevelService.kt
+++ mall-service/mall-member/src/main/kotlin/com/github/member/service/MemberLevelService.kt
@@ -3,15 +3,17 @@ package com.github.member.service
 import com.github.member.dao.MemberLevelDao
 import com.github.member.entity.MemberLevel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.toList
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 
 /**
-*
-* @author Andochiwa
-* @email a1066079469@gmail.com
-* @date 2021-09-26 03:43:36
-*/
+ *
+ * @author Andochiwa
+ * @email a1066079469@gmail.com
+ * @date 2021-09-26 03:43:36
+ */
 @Service
 class MemberLevelService {
     @Autowired
@@ -22,15 +24,34 @@ class MemberLevelService {
     }
 
     suspend fun saveOrUpdate(memberLevel: MemberLevel): MemberLevel {
+        println(memberLevel)
         return memberLevelDao.save(memberLevel)
     }
 
-    suspend fun deleteById(id: Long) {
-        memberLevelDao.deleteById(id)
+    suspend fun deleteById(ids: List<Long>) {
+        memberLevelDao.deleteAllById(ids)
     }
 
     fun getAll(): Flow<MemberLevel> {
         return memberLevelDao.findAll()
+    }
+
+
+    suspend fun getAllPagination(page: Int, limit: Int, key: String?): Map<String, Any> {
+        val count: Long
+        val resultList: List<MemberLevel>
+        val pageRequest = PageRequest.of(page, limit)
+        if (key == null) {
+            resultList = memberLevelDao.findAllBy(pageRequest).toList()
+            count = memberLevelDao.count()
+        } else {
+            resultList = memberLevelDao.findAllByNameContaining(key, pageRequest).toList()
+            count = memberLevelDao.countAllByNameContaining(key)
+        }
+        return mutableMapOf<String, Any>().apply {
+            this["list"] = resultList
+            this["totalCount"] = count
+        }
     }
 }
 
