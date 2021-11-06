898c0660c4ede97e389c3f3e161d6d28bb99392c
andochiwa <a1066079469@gmail.com>
2021-11-06 19:24:03 +0900

    [feat] sku elasticsearch data TO

---
+++ mall-common/common_utils/src/main/kotlin/com/github/to/es/SkuEsTo.kt
@@ -0,0 +1,28 @@
+package com.github.to.es
+
+import jdk.internal.util.xml.impl.Attrs
+import java.math.BigDecimal
+
+
+data class SkuEsTo(
+    var skuId: Long? = null,
+    var spuId: Long? = null,
+    var skuTitle: String? = null,
+    var skuPrice: BigDecimal? = null,
+    var skuImg: String? = null,
+    var saleCount: Long? = null,
+    var hasStock: Boolean = false,
+    var hotScore: Long? = null,
+    var brandId: Long? = null,
+    var catelogId: Long? = null,
+    var brandName: String? = null,
+    var brandImg: String? = null,
+    var catelogName: String? = null,
+    var attrs: MutableList<Attrs>? = null
+) {
+    data class Attrs(
+        var attrId: Long? = null,
+        var attrName: String? = null,
+        var attrValue: String? = null
+    )
+}
