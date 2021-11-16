ef8a2f17baa48a6fc250ab40d52495f52d61b607
andochiwa <a1066079469@gmail.com>
2021-11-16 23:38:10 +0900

    [fix] search param vo type

--- mall-service/mall-search/src/main/kotlin/com/github/search/vo/SearchParamVo.kt
+++ mall-service/mall-search/src/main/kotlin/com/github/search/vo/SearchParamVo.kt
@@ -2,11 +2,11 @@ package com.github.search.vo
 
 data class SearchParamVo(
     var keyword: String? = null,
-    var catelog3Id: Int? = null,
+    var catelog3Id: String? = null,
     var sort: String? = null, // sort=saleCount_asc
     var hasStock: Int? = null,
     var skuPrice: String? = null, // skuPrice=1_500
-    var brandId: List<Long>? = null,
+    var brandId: List<String>? = null,
     var attrs: List<String>? = null, // attrs=2_白色:黑色
-    var pageNum: Int? = null
+    var pageNum: Int = 1
 )
