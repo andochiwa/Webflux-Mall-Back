6658cfc101902f535334d5c4dd32777faf92b8f7
andochiwa <a1066079469@gmail.com>
2021-12-01 05:32:09 +0900

    [fix] optimize import

--- mall-service/mall-product/src/main/kotlin/com/github/product/web/IndexController.kt
+++ mall-service/mall-product/src/main/kotlin/com/github/product/web/IndexController.kt
@@ -1,13 +1,10 @@
 package com.github.product.web
 
-import com.github.dto.ResultDto
-import com.github.dto.resultSuccess
 import com.github.product.service.CategoryService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseBody
 
 /**
  * @author Andochiwa
