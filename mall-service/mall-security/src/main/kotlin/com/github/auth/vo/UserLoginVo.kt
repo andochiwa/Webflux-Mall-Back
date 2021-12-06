1a446e406159e1461068a6d36af9942b6d89685d
andochiwa <a1066079469@gmail.com>
2021-12-06 20:50:24 +0900

    [feat] login vo valid

--- mall-service/mall-security/src/main/kotlin/com/github/auth/vo/UserLoginVo.kt
+++ mall-service/mall-security/src/main/kotlin/com/github/auth/vo/UserLoginVo.kt
@@ -1,7 +1,13 @@
 package com.github.auth.vo
 
+import org.hibernate.validator.constraints.Length
+import javax.validation.constraints.NotBlank
+
 data class UserLoginVo(
+    @field:NotBlank
     var account: String? = null,
 
+    @field:NotBlank
+    @field:Length(min = 6, max = 18)
     var password: String? = null
 )
